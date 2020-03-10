94f049c583046a5cfb1a42a5adecaa97a04e4c9e
Konrad Siek <konrad.siek@gmail.com>
2020-03-10 18:59:58 +0100

    Longer lines!

--- code/fml/src/main.rs
+++ code/fml/src/main.rs
@@ -15,40 +15,25 @@ use crate::fml_ast::AST::BooleanLiteral;
 
 #[test]
 fn numbers() {
-    assert_eq!(ExpressionParser::new().parse("0"),
-               Ok(Number(0)));
-    assert_eq!(ExpressionParser::new().parse("-0"),
-               Ok(Number(0)));
-    assert_eq!(ExpressionParser::new().parse("2"),
-               Ok(Number(2)));
-    assert_eq!(ExpressionParser::new().parse("-2"),
-               Ok(Number(-2)));
-    assert_eq!(ExpressionParser::new().parse("42"),
-               Ok(Number(42)));
-    assert_eq!(ExpressionParser::new().parse("042"),
-               Ok(Number(42)));
-    assert_eq!(ExpressionParser::new().parse("00"),
-               Ok(Number(0)));
-    assert_eq!(ExpressionParser::new().parse("-042"),
-               Ok(Number(-42)));
-    assert_eq!(ExpressionParser::new().parse("-00"),
-               Ok(Number(0)));
+    assert_eq!(ExpressionParser::new().parse("0"), Ok(Number(0)));
+    assert_eq!(ExpressionParser::new().parse("-0"), Ok(Number(0)));
+    assert_eq!(ExpressionParser::new().parse("2"), Ok(Number(2)));
+    assert_eq!(ExpressionParser::new().parse("-2"), Ok(Number(-2)));
+    assert_eq!(ExpressionParser::new().parse("42"), Ok(Number(42)));
+    assert_eq!(ExpressionParser::new().parse("042"), Ok(Number(42)));
+    assert_eq!(ExpressionParser::new().parse("00"), Ok(Number(0)));
+    assert_eq!(ExpressionParser::new().parse("-042"), Ok(Number(-42)));
+    assert_eq!(ExpressionParser::new().parse("-00"), Ok(Number(0)));
 }
 
 #[test]
 fn identifiers() {
-    assert_eq!(ExpressionParser::new().parse("_"),
-               Ok(Identifier("_")));
-    assert_eq!(ExpressionParser::new().parse("_x"),
-               Ok(Identifier("_x")));
-    assert_eq!(ExpressionParser::new().parse("x"),
-               Ok(Identifier("x")));
-    assert_eq!(ExpressionParser::new().parse("x1"),
-               Ok(Identifier("x1")));
-    assert_eq!(ExpressionParser::new().parse("spaceship"),
-               Ok(Identifier("spaceship")));
-    assert_eq!(ExpressionParser::new().parse("___"),
-               Ok(Identifier("___")));
+    assert_eq!(ExpressionParser::new().parse("_"), Ok(Identifier("_")));
+    assert_eq!(ExpressionParser::new().parse("_x"), Ok(Identifier("_x")));
+    assert_eq!(ExpressionParser::new().parse("x"), Ok(Identifier("x")));
+    assert_eq!(ExpressionParser::new().parse("x1"), Ok(Identifier("x1")));
+    assert_eq!(ExpressionParser::new().parse("spaceship"), Ok(Identifier("spaceship")));
+    assert_eq!(ExpressionParser::new().parse("___"), Ok(Identifier("___")));
 }
 
 #[test]
