3c09921981872cc1ca21bbb16ef1d2b4cc91e4b6
Konrad Siek <konrad.siek@gmail.com>
2020-03-10 17:11:10 +0100

    Identifiers.

--- code/fml/src/fml_ast.rs
+++ code/fml/src/fml_ast.rs
@@ -2,16 +2,17 @@ use std::fmt::{Debug, Error, Formatter};
 use std::cmp::PartialEq;
 
 #[derive(PartialEq)]
-pub enum AST/*<'ast>*/ {
+pub enum AST<'ast> {
     Number(i32),
-    //Identifier(&'ast str),
+    Identifier(&'ast str),
 }
 
-impl Debug for AST {
+impl Debug for AST<'_> {
     fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
         use self::AST::*;
         match *self {
             Number(n) => write!(fmt, "Number({:?})", n),
+            Identifier(id) => write!(fmt, "Identifier({})", id),
             //Op(ref l, op, ref r) => write!(fmt, "({:?} {:?} {:?})", l, op, r),
             //Error => write!(fmt, "error"),
         }
--- code/fml/src/main.rs
+++ code/fml/src/main.rs
@@ -6,44 +6,44 @@ lalrpop_mod!(pub fml); // syntesized by LALRPOP
 
 use crate::fml::ExpressionParser;
 use crate::fml_ast::AST::Number;
+use crate::fml_ast::AST::Identifier;
 
 #[test]
- fn number_0() {
+fn numbers() {
     assert_eq!(ExpressionParser::new().parse("0"),
-               Ok(Box::new(Number(0))));
-}
-
-#[test]
-fn numer_() {
-    assert_eq!(ExpressionParser::new().parse("0"),
-               Ok(Box::new(Number(0))));
+               Ok(Number(0)));
     assert_eq!(ExpressionParser::new().parse("-0"),
-               Ok(Box::new(Number(0))));
+               Ok(Number(0)));
     assert_eq!(ExpressionParser::new().parse("2"),
-               Ok(Box::new(Number(2))));
+               Ok(Number(2)));
     assert_eq!(ExpressionParser::new().parse("-2"),
-               Ok(Box::new(Number(-2))));
+               Ok(Number(-2)));
     assert_eq!(ExpressionParser::new().parse("42"),
-               Ok(Box::new(Number(42))));
+               Ok(Number(42)));
     assert_eq!(ExpressionParser::new().parse("042"),
-               Ok(Box::new(Number(42))));
+               Ok(Number(42)));
     assert_eq!(ExpressionParser::new().parse("00"),
-               Ok(Box::new(Number(0))));
+               Ok(Number(0)));
     assert_eq!(ExpressionParser::new().parse("-042"),
-               Ok(Box::new(Number(-42))));
+               Ok(Number(-42)));
     assert_eq!(ExpressionParser::new().parse("-00"),
-               Ok(Box::new(Number(0))));
+               Ok(Number(0)));
 }
 
 #[test]
 fn identifiers() {
-    assert!(ExpressionParser::new().parse("_").is_ok());
-    assert!(ExpressionParser::new().parse("_0").is_ok());
-    assert!(ExpressionParser::new().parse("_x").is_ok());
-    assert!(ExpressionParser::new().parse("x").is_ok());
-    assert!(ExpressionParser::new().parse("x1").is_ok());
-    assert!(ExpressionParser::new().parse("spaceship").is_ok());
-    assert!(ExpressionParser::new().parse("___").is_ok());
+    assert_eq!(ExpressionParser::new().parse("_"),
+               Ok(Identifier("_")));
+    assert_eq!(ExpressionParser::new().parse("_x"),
+               Ok(Identifier("_x")));
+    assert_eq!(ExpressionParser::new().parse("x"),
+               Ok(Identifier("x")));
+    assert_eq!(ExpressionParser::new().parse("x1"),
+               Ok(Identifier("x1")));
+    assert_eq!(ExpressionParser::new().parse("spaceship"),
+               Ok(Identifier("spaceship")));
+    assert_eq!(ExpressionParser::new().parse("___"),
+               Ok(Identifier("___")));
 }
 
 #[cfg(not(test))]
