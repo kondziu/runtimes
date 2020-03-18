488260b9549a1973ae37da15e7426f97e270bf1a
Konrad Siek <konrad.siek@gmail.com>
2020-03-18 01:16:11 +0100

    Output AST as JSON, S-expressions, or YAML.

--- code/fml/src/main.rs
+++ code/fml/src/main.rs
@@ -1,8 +1,11 @@
 #[macro_use]
 extern crate lalrpop_util;
 //extern crate unescape;
+
 extern crate serde;
 extern crate serde_lexpr;
+extern crate serde_json;
+extern crate serde_yaml;
 
 #[macro_use]
 pub mod fml_ast;
@@ -818,12 +821,41 @@ fn test_object_with_many_members() {
 #[cfg(not(test))]
 fn main() {
     use std::io::{self, Read};
-    let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer);
+    use std::collections::HashMap;
+    use std::env;
 
-    //let input = "let x = 1; let y = x + 2; print(\"~\\n\", x + y)";
-    match TopLevelParser::new().parse(buffer.as_str()) {
-        Ok(ast) => println!("{}", serde_lexpr::to_string(&ast).unwrap()),
-        Err(message) => println!("Parse error: {}", message),
+    type Converter = fn(&AST) -> String;
+    let converters: HashMap<String, Converter> = {
+        let mut converters: HashMap<String, Converter> = HashMap::new();
+        converters.insert(String::from("--lisp"),
+                          |ast| serde_lexpr::to_string(&ast).unwrap());
+        converters.insert(String::from("--json"),
+                          |ast| serde_json::to_string(&ast).unwrap());
+        converters.insert(String::from("--yaml"),
+                          |ast| serde_yaml::to_string(&ast).unwrap());
+        converters
+    };
+
+    let (flags, _ /*files*/): (Vec<_>, Vec<_>) =
+        env::args().into_iter().partition(|e| e.starts_with("--"));
+
+    let mut buffer = String::new();
+    if io::stdin().read_to_string(&mut buffer).is_err() {
+        println!("Cannot read stdin.");
+        return;
     }
+
+    let ast: AST = match TopLevelParser::new().parse(buffer.as_str()) {
+        Ok(ast) => ast,
+        Err(message) => {
+            println!("Parse error: {}", message);
+            AST::Unit
+        },
+    };
+
+    flags.iter().for_each(|e| if converters.contains_key(e) {
+        println!("{}", converters.get(e).unwrap()(&ast));
+    } else {
+        println!("Unknown flag: {}", e);
+    });
 }
