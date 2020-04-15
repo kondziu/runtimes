b729cebf31082bc521b3352c50d31281cca01999
Konrad Siek <konrad.siek@gmail.com>
2020-04-15 15:16:39 +0200

    Actually, most things should be u32.

--- code/fml/bytecode/src/types.rs
+++ code/fml/bytecode/src/types.rs
@@ -2,39 +2,40 @@ use crate::serializable::Serializable;
 use crate::serializable;
 use std::io::{Read, Write};
 
-#[derive(PartialEq,Debug,Copy,Clone)] pub struct Size(usize);
+#[derive(PartialEq,Debug,Copy,Clone)] pub struct Size(u32);
 #[derive(PartialEq,Debug,Copy,Clone)] pub struct Address(u64);
-#[derive(PartialEq,Debug,Copy,Clone)] pub struct ConstantPoolIndex(u64);
-#[derive(PartialEq,Debug,Copy,Clone)] pub struct LocalFrameIndex(u64);
+#[derive(PartialEq,Debug,Copy,Clone)] pub struct ConstantPoolIndex(u32);
+#[derive(PartialEq,Debug,Copy,Clone)] pub struct LocalFrameIndex(u32);
 
-impl Size              { pub fn new(value: usize) -> Size              { Size(value)              }}
-impl Address           { pub fn new(value: u64)   -> Address           { Address(value)           }}
-impl ConstantPoolIndex { pub fn new(value: u64)   -> ConstantPoolIndex { ConstantPoolIndex(value) }}
-impl LocalFrameIndex   { pub fn new(value: u64)   -> LocalFrameIndex   { LocalFrameIndex(value)   }}
+impl Size              { pub fn new(value: u32) -> Size              { Size(value)              }}
+impl Address           { pub fn new(value: u64) -> Address           { Address(value)           }}
+impl ConstantPoolIndex { pub fn new(value: u32) -> ConstantPoolIndex { ConstantPoolIndex(value) }}
+impl LocalFrameIndex   { pub fn new(value: u32) -> LocalFrameIndex   { LocalFrameIndex(value)   }}
 
 impl Serializable for Size {
 
     fn serialize<W: Write> (&self, sink: &mut W) -> () {
-        let size_of_type = std::mem::size_of::<usize>();
-        match size_of_type {
-            1 => serializable::write_u8(sink, self.0 as u8),
-            4 => serializable::write_u32(sink, self.0 as u32),
-            8 => serializable::write_u64(sink, self.0 as u64),
-            _ => panic!("Cannot serialize: sizeof::<usize> == {} \
-                         but only 1, 4, and 8 are supported", size_of_type),
-        }
+//        let size_of_type = std::mem::size_of::<usize>();
+//        match size_of_type {
+//            1 => serializable::write_u8(sink, self.0 as u8),
+//            4 => serializable::write_u32(sink, self.0 as u32),
+//            8 => serializable::write_u64(sink, self.0 as u64),
+//            _ => panic!("Cannot serialize: sizeof::<usize> == {} \
+//                         but only 1, 4, and 8 are supported", size_of_type),
+//        }
+        serializable::write_u32(sink, self.0)
     }
 
     fn from_bytes<R: Read>(input: &mut R) -> Self {
-        let size_of_type = std::mem::size_of::<usize>();
-        let value: usize = match size_of_type {
-            1 => serializable::read_u8(input) as usize,
-            4 => serializable::read_u32(input) as usize,
-            8 => serializable::read_u64(input) as usize,
-            _ => panic!("Cannot deserialize: sizeof::<usize> == {} \
-                         but only 1, 4, and 8 are supported", size_of_type),
-        };
-        Size(value)
+//        let size_of_type = std::mem::size_of::<usize>();
+//        let value: usize = match size_of_type {
+//            1 => serializable::read_u8(input) as usize,
+//            4 => serializable::read_u32(input) as usize,
+//            8 => serializable::read_u64(input) as usize,
+//            _ => panic!("Cannot deserialize: sizeof::<usize> == {} \
+//                         but only 1, 4, and 8 are supported", size_of_type),
+//        };
+        Size(serializable::read_u32(input))
     }
 }
 
@@ -49,18 +50,18 @@ impl Serializable for Address {
 
 impl Serializable for ConstantPoolIndex {
     fn serialize<W: Write> (&self, sink: &mut W) -> () {
-        serializable::write_u64(sink, self.0)
+        serializable::write_u32(sink, self.0)
     }
     fn from_bytes<R: Read>(input: &mut R) -> Self {
-        ConstantPoolIndex(serializable::read_u64(input))
+        ConstantPoolIndex(serializable::read_u32(input))
     }
 }
 
 impl Serializable for LocalFrameIndex {
     fn serialize<W: Write> (&self, sink: &mut W) -> () {
-        serializable::write_u64(sink, self.0)
+        serializable::write_u32(sink, self.0)
     }
     fn from_bytes<R: Read>(input: &mut R) -> Self {
-        LocalFrameIndex(serializable::read_u64(input))
+        LocalFrameIndex(serializable::read_u32(input))
     }
 }
